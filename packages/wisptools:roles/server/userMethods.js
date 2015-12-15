19a3f2b571ec7f29f43582f94b800df30cc0ef66
Vivek Dinesh <vivek@wisptools.com>
2015-12-15 15:20:34 +0530

    Method to append Role to user.

--- packages/wisptools:roles/server/userMethods.js
+++ packages/wisptools:roles/server/userMethods.js
@@ -14,6 +14,18 @@ Meteor.methods({
 
     Roles.setUserRoles(targetUserId, roles);
     Roles.addUsersToRoles(targetUserId,roles);
+  },
+  
+  addRoleToUser: function (targetUserId, role) {
+    var loggedInUser = Meteor.user()
+       
+    if (!loggedInUser ||
+        !Roles.userIsInRole(loggedInUser, 
+                            ['admin'])) {
+      throw new Meteor.Error(403, "Access denied")
+    }
+
+    Roles.addUsersToRoles(targetUserId, role);
   }
   
   
