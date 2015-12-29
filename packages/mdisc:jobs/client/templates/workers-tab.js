35fe0b9875d717bb933c9c371df0a43b9e6dc5a2
Vivek Dinesh <vivek@wisptools.com>
2015-12-29 15:59:03 +0530

    Worked on worker group view page.

--- packages/mdisc:jobs/client/templates/workers-tab.js
+++ packages/mdisc:jobs/client/templates/workers-tab.js
@@ -1,9 +1,23 @@
+var tick = 10000;
+var reactiveDate = new ReactiveVar(new Date());
+
+Meteor.setInterval((function () {
+  return reactiveDate.set(new Date());
+}), tick);
+
 Template.mdJobsViewWorkersTab.helpers({
   tabData: function () {
     var workerTabs = new Array();
     Meteor.users.find({roles: {$in: ['job-server']}}).forEach(function (worker) {
+      var tabIcon = '';
+      if (reactiveDate.get()-worker.profile.lastActive<tick) {
+        tabIcon = '<i class="fa fa-circle pull-right text-success'+'"></i>';
+      } else {
+        tabIcon = '<i class="fa fa-circle pull-right text-danger'+'"></i>';
+      }
       workerTabs.push({
         tabName: worker.profile.jobTag,
+        tabIcon: tabIcon,
         tabId: "worker-tab-"+workerTabs.length,
         tabTemplate: "mdJobsViewWorkersTabContent",
         worker: worker
@@ -14,5 +28,14 @@ Template.mdJobsViewWorkersTab.helpers({
       title: "Workers",
       pages: workerTabs
     };
+  }
+});
+
+Template.mdJobsViewWorkersTabContent.helpers({
+  isOnline: function (lastActive) {
+    if (reactiveDate.get() - lastActive < tick) {
+      return true;
+    }
+    return false;
   }
 });
